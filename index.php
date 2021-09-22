046d3e1df645f006a63666a53e2054f234fc1020
AnselmoLupague <lupagueanselmo@gmail.com>
2021-09-22 19:47:55 +0800

    Added Login Page
    +login
    +logout
    +db & sql
    +images

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php 
+//initialize session
+  session_start();
 
+  if( !isset($_SESSION['email']) || empty($_SESSION['email'])){
+    header('location: login.php');
+    exit;
+  }
 ?>
 
 <html> 
